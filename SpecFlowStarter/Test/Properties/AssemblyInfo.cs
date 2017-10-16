751b44ddb1da845f0fc97139746964bd565b15ce
Agile Institute <Rob.Myers@agileInstitute.com>
2017-10-16 15:48:09 -0700

    Adding VisualStudio.Net fundamentals.
    NOTE: Yu should be able to simply open the .sln file from here on out!
    :-)

---
+++ SpecFlowStarter/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d2344f0e-0dd2-41d8-9804-d96745d6d80f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
